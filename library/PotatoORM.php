1a0c0d8a5955dd5881e5f22ddaf5839176792093
Samuel Gachanja <gksamuel1@gmail.com>
2016-03-26 16:29:38 +0300

    Adding Documentation and refinements to potatoORM library

--- library/PotatoORM.php
+++ library/PotatoORM.php
@@ -1,56 +1,71 @@
 <?php
 
 /**
- * Description of PotatoORM
- *
+ * Databaase abstraction library
  * @author samuel gachanja <gksamuel1@gmail.com>
  * @version 1.0 March 2016
- * @category library
+ * @category model
+ */
+
+/**
+ * Contains functions to interact with the database
  */
 class PotatoORM {
-    /*
+
+    /**
      * Constructor method for potatoORM class
      * Creates database object
      */
-
     public function __construct() {
-        $this->db = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+        try {
+            $this->db = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+        } catch (Exception $e) {
+            echo "Could not  connect to database";
+            die();
+        }
     }
 
-    /*
+    /**
      * getAll method - fetches all records from the table.
      * @param none
-     * @returns n/a
+     * @returns true if records were retrieved false if unable to get records
      * 
      */
-
     function getAll() {
         $classname = get_class($this);
         $statement = $this->db->prepare("SELECT * FROM $classname");
         $statement->execute();
         $this->$classname = (array) $statement->fetchAll();
+        if ($statement->errorInfo()[0] == '00000') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /*
-     * destroy method - deletes a record from a table
-     * @params id - the primary key of the record to be deleted.
-     * @returns n/a
+    /**
+      destroy method - deletes a record from a table
+      @returns true if records were retrieved false if unable to get records
+      @param integer $id - the primary key of the record to be deleted.
      */
-
     function destroy($id) {
         $primaryKey = $this->getKey(get_class($this));
         $classname = get_class($this);
         $statement = $this->db->prepare("delete from $classname where $primaryKey = $id");
         $statement->execute();
         $this->error = $statement->errorInfo();
+        if ($statement->rowCount() == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /*
+    /**
      * find method - retreives a record from a table
      * @param $id - the primary key of the record to be retrieved.
-     * @returns n/a
+     * @returns true if records was found, false if record was not found
      */
-
     function find($id) {
         $primaryKey = $this->getKey(get_class($this));
         $classname = get_class($this);
@@ -62,13 +77,18 @@ class PotatoORM {
                 $this->$name = $row[$name];
             }
         }
+        if ($statement->rowCount() == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /*
+    /**
      * save method - updates a record in the table.
      * if the record does not exist it will create a new record.
+     * @returns true if record was saved, false if unable to save record
      */
-
     function save() {
         try {
             $primaryKey = $this->getKey(get_class($this));
@@ -82,26 +102,32 @@ class PotatoORM {
                 $error = $statement->errorInfo();
                 if ($error[0] == "00000") {
                     $this->$primaryKey = $this->db->lastInsertId();
+                    return true;
                 } else {
                     $this->error = $error;
-                    throw new Exception($error[2]);
+                    return false;
                 }
             } else {
                 $whereclause = $this->getWhereClause($props);
                 $query = "update $classname set $whereclause where  $primaryKey = " . $this->$primaryKey;
                 $statement = $this->db->prepare($query);
                 $statement->execute();
+                if ($statement->rowCount() == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
         } catch (Exception $e) {
-            
+            echo "Could not save record";
         }
     }
 
-    /*
+    /**
      * getKey method - identifies the primary key of a table
      * @param $model - the name of the class. This is assumed to be the name of the table
+     * @return String - the name of the table primary key
      */
-
     function getKey($model) {
         $select = $this->db->query("SELECT * FROM $model limit 1");
         $meta = $select->getColumnMeta(0);
@@ -114,14 +140,14 @@ class PotatoORM {
             }
             $x++;
         }
+        return false;
     }
 
-    /*
+    /**
      * getWhereClause method - this method constucts the where clause of sql update statement.
      * @param $props - an array containing the properties of the table.
      * @return $whereClause - a string containing the where clause of the sql statement.
      */
-
     function getWhereClause($props) {
         $whereclause = "";
         $counter = 0;
@@ -135,12 +161,11 @@ class PotatoORM {
         return $whereclause;
     }
 
-    /*
+    /**
      * getInsertClause method - This method constructs the insert clause of the the sql statement
      * @param $props - an array containing the properties of the table.
      * @return $insert - a string containing the insert clause of the sql statement.
      */
-
     function getInsertClause($props) {
         $columns = "";
         $values = "";
