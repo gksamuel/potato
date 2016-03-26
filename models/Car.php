a83873c53da69b95bdf27c86b56f89efc2b8166a
Samuel Gachanja <gksamuel1@gmail.com>
2016-03-26 16:28:35 +0300

    Adding Documentation to models

--- models/Car.php
+++ models/Car.php
@@ -1,17 +1,38 @@
 <?php
 
-require_once 'library/PotatoORM.php';
 /**
  * This is a stub model class for Car
  * Functions specific to the Car class come here
- *
+ */
+/**
  * @author samuel gachanja <gksamuel1@gmail.com>
  * @version 1.0 March 2016
  * @category model
  */
+require_once 'library/PotatoORM.php';
+
+/**
+ * Stores properties of the Car class
+ * Functions specific to Class car come in this file.
+ * @class Car
+ */
 class Car extends PotatoORM {
-    public  $carID;
-    public  $name;
-    public  $price;
+
+    /**
+     *
+     * @var type carID is the primary key for the Car class
+     */
+    public $carID;
+
+    /**
+     *
+     * @var type name stores the name of the car
+     */
+    public $name;
+
+    /**
+     * @var type price stores the price of the car
+     */
+    public $price;
 
 }
